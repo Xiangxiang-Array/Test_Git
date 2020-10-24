d8c3732d894136d359fdea8f9effd320d06b15a8
刘云岑 <1850697175@qq.com>
2020-10-24 14:34:53 +0800

    测试第一次提交

---
+++ src/com/xiang/Main.java
@@ -0,0 +1,14 @@
+package com.xiang;
+
+/**
+ * @author Xiang想
+ * @title: Main 测试
+ * @projectName Test_Git
+ * @description: TODO
+ * @date 2020/10/24  10:05
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("创建了这个项目");
+    }
+}
