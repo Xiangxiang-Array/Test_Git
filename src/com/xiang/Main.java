d29283a0ddaf319560d9739ad8be462e4887dd89
刘云岑 <1850697175@qq.com>
2020-10-24 14:57:13 +0800

    主干 在创建分支后 第二次修改

--- src/com/xiang/Main.java
+++ src/com/xiang/Main.java
@@ -13,5 +13,6 @@ public class Main {
         System.out.println("第二次提交");
         System.out.println("第三次提交 并创建分支");
         System.out.println("分支修改  第一次提交");
+        System.out.println("分支修改，并未合并代码，第二次提交");
     }
 }
